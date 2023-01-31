932f6053a096bb43bcfaace3ee42d80d2b404452
Tomas Balaz <balaztomas0@gmail.com>
2023-01-31 14:51:34 +0100

    Enable Spring boot admin using annotation

--- src/main/java/sk/balaz/springbootadminserver/Application.java
+++ src/main/java/sk/balaz/springbootadminserver/Application.java
@@ -1,13 +1,15 @@
 package sk.balaz.springbootadminserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootAdminServerApplication {
+@EnableAdminServer
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootAdminServerApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
